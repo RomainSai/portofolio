42b808ce3e0cda94eba46a2060960e754571910e
RomainSAILLOUR <romain.saillour2018@campus-eni.fr>
2019-02-12 13:41:38 +0100

    amelioration navbar et ajout projet portofolio

--- popup-slider.js
+++ popup-slider.js
@@ -52,3 +52,48 @@ $( document ).ready(function() {
     $('.sliderPop2 .ct-sliderPop-container').removeClass('slides');
   });
 });
+
+//Popup Portofolio
+$(document).ready(function () {
+    $('.btn-slider3').on('click', function () {
+        document.getElementById("header").style.filter = " blur(2px)";
+        document.getElementById("first-section").style.filter = " blur(2px)";
+        document.getElementById("projet").style.filter = " blur(2px)";
+        document.getElementById("contact").style.filter = " blur(2px)";
+        $('.sliderPop3').show();
+        $('.ct-sliderPop-container').addClass('open');
+        $('.sliderPop3').addClass('flexslider');
+        $('.sliderPop3 .ct-sliderPop-container').addClass('slides');
+        $('.sliderPop3').flexslider({
+            selector: '.ct-sliderPop-container > .ct-sliderPop',
+            slideshow: false,
+            controlNav: false,
+            controlsContainer: '.ct-sliderPop-container'
+        });
+    });
+
+    $('.ct-sliderPop-close').on('click', function () {
+        document.getElementById("header").style.filter = "none";
+        document.getElementById("first-section").style.filter = "none";
+        document.getElementById("projet").style.filter = "none";
+        document.getElementById("contact").style.filter = "none";
+        $('.sliderPop3').hide();
+        $('.ct-sliderPop-container').removeClass('open');
+        $('.sliderPop3').removeClass('flexslider');
+        $('.sliderPop3 .ct-sliderPop-container').removeClass('slides');
+    });
+});
+
+//Script NAVABAR disparait on scroll reapparait scroll
+$(document).ready(function () {
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar").style.top = "0";
+        } else {
+            document.getElementById("navbar").style.top = "-50px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
+});
